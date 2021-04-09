0f9bb2f4bf4b2453776a6a207ab9d0e0dd4e6e0f
dzemmarat <maratdzhemankulov@gmail.com>
2021-04-09 22:49:52 +0400

    Groupie Test App

---
+++ app/src/main/java/com/mrz/gropietest/GroupieAdapter.kt
@@ -0,0 +1,17 @@
+package com.mrz.gropietest
+
+import android.view.View
+import com.mrz.gropietest.databinding.ItemViewBinding
+import com.xwray.groupie.viewbinding.BindableItem
+
+class GroupieAdapter(private val song: String) : BindableItem<ItemViewBinding>() {
+    override fun initializeViewBinding(view: View): ItemViewBinding {
+        return ItemViewBinding.bind(view)
+    }
+    override fun getLayout() = R.layout.item_view
+
+    override fun bind(viewBinding: ItemViewBinding, position: Int) {
+        viewBinding.tvText.text = song
+    }
+
+}
